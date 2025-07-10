75c02f6c2d7aa3e3a73c11792fcf58e9ee21b681
tomoto-png <hirosi.tomoto0130@gmail.com>
2025-07-10 17:35:29 +0900

    微調整

---
+++ LearnTrack/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\Answer;
+use App\Policies\AnswerPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    protected $policies = [
+        Answer::class => AnswerPolicy::class,
+    ];
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
