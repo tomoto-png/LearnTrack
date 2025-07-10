66eeaaaa0c82bd5d1e3be4f95a95e222be98b63e
tomoto-png <hirosi.tomoto0130@gmail.com>
2025-07-10 17:09:44 +0900

    微調整

--- LearnTrack/resources/views/question/show.blade.php
+++ LearnTrack/resources/views/question/show.blade.php
@@ -237,13 +237,13 @@
                                             <a href="{{ route('replie.create', $answer->id) }}">返信する</a>
                                         </div>
                                     @endcan
-                                    @can('setBest', $answer)
-                                        <button onclick="openBestModal()"
-                                                class="flex items-center gap-1 bg-yellow-500 text-white text-sm px-2 py-1 rounded-md hover:bg-yellow-600 transition">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 -960 960 960" fill="#ffffff"><path d="m387-412 35-114-92-74h114l36-112 36 112h114l-93 74 35 114-92-71-93 71ZM240-40v-309q-38-42-59-96t-21-115q0-134 93-227t227-93q134 0 227 93t93 227q0 61-21 115t-59 96v309l-240-80-240 80Zm240-280q100 0 170-70t70-170q0-100-70-170t-170-70q-100 0-170 70t-70 170q0 100 70 170t170 70ZM320-159l160-41 160 41v-124q-35 20-75.5 31.5T480-240q-44 0-84.5-11.5T320-283v124Zm160-62Z"/></svg>
-                                            ベストアンサー
-                                        </button>
-                                    @endcan
+                                    {{-- @can('setBest', $answer) --}}
+                                    <button onclick="openBestModal()"
+                                            class="flex items-center gap-1 bg-yellow-500 text-white text-sm px-2 py-1 rounded-md hover:bg-yellow-600 transition">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 -960 960 960" fill="#ffffff"><path d="m387-412 35-114-92-74h114l36-112 36 112h114l-93 74 35 114-92-71-93 71ZM240-40v-309q-38-42-59-96t-21-115q0-134 93-227t227-93q134 0 227 93t93 227q0 61-21 115t-59 96v309l-240-80-240 80Zm240-280q100 0 170-70t70-170q0-100-70-170t-170-70q-100 0-170 70t-70 170q0 100 70 170t170 70ZM320-159l160-41 160 41v-124q-35 20-75.5 31.5T480-240q-44 0-84.5-11.5T320-283v124Zm160-62Z"/></svg>
+                                        ベストアンサー
+                                    </button>
+                                    {{-- @endcan --}}
                                 </div>
 
                                 {{-- 回答の返信一覧 --}}
